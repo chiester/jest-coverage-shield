fbb82e7aadb53897ab17b69d0836ba89df9ab3ca
Patrick Lai <patrickpakhanglai.lai@iag.com.au>
2018-07-27 03:02:43 +1000

    Safer way to replace percentage

--- test/cloverReportParser-test.js
+++ test/cloverReportParser-test.js
@@ -1,21 +1,21 @@
-'use strict'
+'use strict';
 
-const expect = require('chai').expect
-const cloverReportParser = require('../lib/cloverReportParser')
-const xmlFileToJson = require('../lib/xmlFileToJson')
+const expect = require('chai').expect;
+const cloverReportParser = require('../lib/cloverReportParser');
+const xmlFileToJson = require('../lib/xmlFileToJson');
 
 describe('Coverage Parser', () => {
   it('should parse clover file.', done => {
-    const reportFilePath = __dirname + '/fixture/clover.xml'
+    const reportFilePath = __dirname + '/fixture/clover.xml';
 
     xmlFileToJson(reportFilePath)
       .then(cloverReportParser)
       .then(report => {
-        expect(report.overallPercent).to.equal(24)
-        expect(report.functionRate).to.equal(0.20665083135391923)
-        expect(report.lineRate).to.equal(0.3381226053639847)
-        expect(report.branchRate).to.equal(0.1928721174004193)
-        done()
-      })
-  })
-})
+        expect(report.overallPercent).to.equal(24);
+        expect(report.functionRate).to.equal(0.20665083135391923);
+        expect(report.lineRate).to.equal(0.3381226053639847);
+        expect(report.branchRate).to.equal(0.1928721174004193);
+        done();
+      });
+  });
+});
--- test/xmlFileToJson-test.js
+++ test/xmlFileToJson-test.js
@@ -1,27 +1,25 @@
-'use strict'
+'use strict';
 
-const expect = require('chai').expect
-const cloverReportParser = require('../lib/cloverReportParser')
-const xmlFileToJson = require('../lib/xmlFileToJson')
+const expect = require('chai').expect;
+const cloverReportParser = require('../lib/cloverReportParser');
+const xmlFileToJson = require('../lib/xmlFileToJson');
 
 describe('XML file to JSON loader', () => {
   it('Should load file as json', done => {
-    const reportFilePath = __dirname + '/fixture/clover.xml'
+    const reportFilePath = __dirname + '/fixture/clover.xml';
 
-    xmlFileToJson(reportFilePath)
-      .then(data => {
-        expect(data).to.be.defined
-        done()
-      })
-  })
+    xmlFileToJson(reportFilePath).then(data => {
+      expect(data).to.be.defined;
+      done();
+    });
+  });
 
   it('Should handle if file not found', done => {
-    const reportFilePath = __dirname + '/non-existent.xml'
+    const reportFilePath = __dirname + '/non-existent.xml';
 
-    xmlFileToJson(reportFilePath)
-      .catch(err => {
-        expect(err).to.be.defined
-        done()
-      })
-  })
-})
+    xmlFileToJson(reportFilePath).catch(err => {
+      expect(err).to.be.defined;
+      done();
+    });
+  });
+});
